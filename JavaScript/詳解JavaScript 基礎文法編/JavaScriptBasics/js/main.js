6a14258408ad69d3002d83fef6ee73fabe6adf36
山本紗也果 <strsyk0505xx@gmail.com>
2020-04-01 15:34:44 +0900

    フォルダ分け

---
+++ JavaScript/詳解JavaScript 基礎文法編/JavaScriptBasics/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+function sum (a, b, c) {
+	// console.log(a + b + c);
+	return a + b + c;
+}
+
+// sum(1, 2, 3);
+// sum(3, 4, 5);
+
+const total = sum(1, 2, 3 ) + sum(3, 4, 5);
+console.log(total);
